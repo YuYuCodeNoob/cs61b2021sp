a73eab83dc053f1f9ecc0bb40efd49302f659330
codeNoob <1371916369@qq.com>
2024-03-01 02:14:46 +0800

    add some CurrentTimeTest

--- proj1/deque/LinkedListDequeTest.java
+++ proj1/deque/LinkedListDequeTest.java
@@ -1,5 +1,6 @@
 package deque;
 
+import edu.princeton.cs.algs4.Stopwatch;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -13,9 +14,6 @@ public class LinkedListDequeTest {
      *
      * && is the "and" operation. */
     public void addIsEmptySizeTest() {
-
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
 		assertTrue("A newly initialized LLDeque should be empty", lld1.isEmpty());
@@ -34,15 +32,11 @@ public class LinkedListDequeTest {
 
 		System.out.println("Printing out deque: ");
 		lld1.printDeque();
-		*/
     }
 
     @Test
     /** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
     public void addRemoveTest() {
-
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 		// should be empty
 		assertTrue("lld1 should be empty upon initialization", lld1.isEmpty());
@@ -54,7 +48,6 @@ public class LinkedListDequeTest {
 		lld1.removeFirst();
 		// should be empty
 		assertTrue("lld1 should be empty after removal", lld1.isEmpty());
-        */
     }
 
     @Test
@@ -62,7 +55,6 @@ public class LinkedListDequeTest {
     public void removeEmptyTest() {
 
         System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<>();
         lld1.addFirst(3);
 
@@ -77,14 +69,12 @@ public class LinkedListDequeTest {
         errorMsg += "  actual size() returned 0\n";
 
         assertEquals(errorMsg, 0, size);
-        */
     }
 
     @Test
     /* Check if you can create LinkedListDeques with different parameterized types*/
     public void multipleParamTest() {
 
-        /*
         LinkedListDeque<String>  lld1 = new LinkedListDeque<String>();
         LinkedListDeque<Double>  lld2 = new LinkedListDeque<Double>();
         LinkedListDeque<Boolean> lld3 = new LinkedListDeque<Boolean>();
@@ -96,15 +86,11 @@ public class LinkedListDequeTest {
         String s = lld1.removeFirst();
         double d = lld2.removeFirst();
         boolean b = lld3.removeFirst();
-        */
     }
 
     @Test
     /* check if null is return when removing from an empty LinkedListDeque. */
     public void emptyNullReturnTest() {
-
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 
         boolean passed1 = false;
@@ -112,15 +98,11 @@ public class LinkedListDequeTest {
         assertEquals("Should return null when removeFirst is called on an empty Deque,", null, lld1.removeFirst());
         assertEquals("Should return null when removeLast is called on an empty Deque,", null, lld1.removeLast());
 
-        */
     }
 
     @Test
     /* Add large number of elements to deque; check if order is correct. */
     public void bigLLDequeTest() {
-
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
         for (int i = 0; i < 1000000; i++) {
             lld1.addLast(i);
@@ -134,6 +116,68 @@ public class LinkedListDequeTest {
             assertEquals("Should have the same value", i, (double) lld1.removeLast(), 0.0);
         }
 
-        */
+    }
+    @Test
+    public void getTest(){
+        LinkedListDeque<Integer> TestDeque = new LinkedListDeque<>();
+        TestDeque.addFirst(1);
+        TestDeque.addFirst(2);
+        TestDeque.addLast(3);
+        int expect0 = 2;
+        int expect1 = 1;
+        int expect2 = 3;
+        assertEquals((Integer) expect0,TestDeque.get(0));
+        assertEquals((Integer) expect1,TestDeque.get(1));
+        assertEquals((Integer) expect2,TestDeque.get(2));
+    }
+    @Test
+    public void testCurrentTime(){
+        LinkedListDeque<Integer> Ns = new LinkedListDeque<>();
+        LinkedListDeque<Double> timePerOpps = new LinkedListDeque<>();
+        int N = 1000;
+        for (int i = 0; i < 10; i++) {
+            Ns.addLast(N);
+            LinkedListDeque<Integer> testModel = new LinkedListDeque<>();
+            Stopwatch sw = new Stopwatch();
+            for (int j = 0; j < N ; j++) {
+                testModel.addLast(j);
+            }
+            double v = sw.elapsedTime();
+            timePerOpps.addLast(v);
+            N *=2;
+        }
+        printTimingTable(Ns,timePerOpps,Ns);
+    }
+    @Test
+    public void TestRemoveCurrentTime(){
+        LinkedListDeque<Integer> Ns = new LinkedListDeque<>();
+        LinkedListDeque<Double> time = new LinkedListDeque<>();
+        int N = 1000;
+        for (int i = 0; i < 10; i++) {
+            Ns.addLast(N);
+            LinkedListDeque<Integer> testModel = new LinkedListDeque<>();
+            for (int j = 0; j < N ; j++) {
+                testModel.addFirst(j);
+            }
+            Stopwatch sw = new Stopwatch();
+            for (int j = 0; j < N; j++) {
+                testModel.removeLast();
+            }
+            double v = sw.elapsedTime();
+            time.addLast(v);
+            N *=2;
+        }
+        printTimingTable(Ns,time,Ns);
+    }
+    private static void printTimingTable(LinkedListDeque<Integer> Ns, LinkedListDeque<Double> times, LinkedListDeque<Integer> opCounts) {
+        System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
+        System.out.printf("------------------------------------------------------------\n");
+        for (int i = 0; i < Ns.size(); i += 1) {
+            int N = Ns.get(i);
+            double time = times.get(i);
+            int opCount = opCounts.get(i);
+            double timePerOp = time / opCount * 1e6;
+            System.out.printf("%12d %12.2f %12d %12.2f\n", N, time, opCount, timePerOp);
+        }
     }
 }
