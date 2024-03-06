a607403af8cbffe0c8368bc277c4e466e588602e
codeNoob <1371916369@qq.com>
2024-03-06 23:38:28 +0800

    finish lab03

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,42 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove(){
+        BuggyAList<Integer> testList = new BuggyAList<>();
+        testList.addLast(4);
+        testList.addLast(4);
+        testList.addLast(4);
+        Integer first = testList.removeLast();
+        Integer second = testList.removeLast();
+        Integer third = testList.removeLast();
+        assertEquals(first,second);
+        assertEquals(third,second);
+        assertEquals(first,third);
+    }
+    @Test
+    public void randomizedTest(){
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> L2 = new BuggyAList<>();
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 3);
+            int size = L.size();
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                L2.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1 && size > 0) {
+                // size
+                Integer last = L.getLast();
+                Integer last2 = L2.getLast();
+                System.out.println(last);
+            }else if (operationNumber == 2 && size > 0){
+                L.removeLast();
+                L2.removeLast();
+            }
+        }
+    }
 }
